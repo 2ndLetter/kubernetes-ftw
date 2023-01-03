13435e2a0762e1b83a31706d67c1ec5a222c157c
Brian <b.matthew.chadwick@gmail.com>
2023-01-02 21:03:12 -0500

    Updated notes

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 # Easily push changes to README.md
-git add . && git commit -m "Updated notes" && git push && clear
+#git add . && git commit -m "Updated notes" && git push && clear
 
+git add .
+
+git commit -m "Updated notes"
+
+git push
+
+clear
